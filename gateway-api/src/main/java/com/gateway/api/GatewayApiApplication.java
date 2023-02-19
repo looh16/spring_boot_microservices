c0489da8c9c1634549b5f5bf9f9a6a6fd65aa235
looh16 <langacustodio@gmail.com>
2023-02-19 12:47:10 +0200

    Add api gateway

---
+++ gateway-api/src/main/java/com/gateway/api/GatewayApiApplication.java
@@ -0,0 +1,14 @@
+package com.gateway.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApiApplication.class, args);
+	}
+
+}
