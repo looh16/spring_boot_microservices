7d924de7cbc95f54c97a55c56ba7a7bfc0e381bd
looh16 <langacustodio@gmail.com>
2023-02-21 16:13:06 +0200

    Add configurations to encrpty password

--- users-api/src/main/java/com/users/api/services/UserServiceImpl.java
+++ users-api/src/main/java/com/users/api/services/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.users.api.data.UserEntity;
@@ -16,17 +17,20 @@ public class UserServiceImpl implements IUserService{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public UserDTO createUser(UserDTO userDTO) {
+		
+		userDTO.setUserId(UUID.randomUUID().toString());
+		userDTO.setEncryptedPassord(bCryptPasswordEncoder.encode(userDTO.getPassword()));
 		
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		UserEntity userEntity = modelMapper.map(userDTO, UserEntity.class);
-		
-		userEntity.setUserId(UUID.randomUUID().toString());
-		userEntity.setEncryptedPassord("test");
 		
 		userRepository.save(userEntity);
 		
