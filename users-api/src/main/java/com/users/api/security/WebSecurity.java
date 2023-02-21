8c6a2977cca748a59ed43c5b0fae35eae1488fb3
looh16 <langacustodio@gmail.com>
2023-02-21 16:43:30 +0200

    Add configurations to accept requests only from API GATEWAY

--- users-api/src/main/java/com/users/api/security/WebSecurity.java
+++ users-api/src/main/java/com/users/api/security/WebSecurity.java
@@ -1,17 +1,25 @@
 package com.users.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
+	
+	@Autowired
+	private Environment environment;
+	
+	
 	
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -19,7 +27,9 @@ public class WebSecurity {
 		http.csrf().disable();
 		
 		http.authorizeHttpRequests()
-		.requestMatchers(HttpMethod.POST, "/users").permitAll()
+		.requestMatchers(HttpMethod.POST, "/users")
+		.access(
+				new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip")+"')"))
 		.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
 		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
