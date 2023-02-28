e929d6102338e2c3cdc4c32723d176796b3f89f4
looh16 <langacustodio@gmail.com>
2023-02-28 18:07:29 +0200

    Add custom filter

--- users-api/src/main/java/com/users/api/security/AuthenticationFilter.java
+++ users-api/src/main/java/com/users/api/security/AuthenticationFilter.java
@@ -15,8 +15,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.userdetails.User;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.users.api.services.IUserService;
